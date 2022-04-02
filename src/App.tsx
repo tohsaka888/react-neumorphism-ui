aa9f7a47344c59e599fcd722cf8c7cfb6927799e
tohsaka888 <156132264@qq.com>
2022-04-02 21:46:35 +0800

    feat: test component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import useBackgroundColor from "./hooks/useBackgroundColor";
 import { BgColorContext } from "./context/BgColorContext";
 import Button from "./components/Button";
 import Modal from "./components/Modal";
+import useTheme from "./hooks/useTheme";
 
 function App() {
-  useBackgroundColor("#814747");
+  useBackgroundColor("#d08383");
   const bgColor = useBackgroundColor();
   const [visible, setVisible] = useState<boolean>(false);
+  const theme = useTheme();
   return (
     <BgColorContext.Provider value={bgColor}>
       <div className="App">
@@ -17,10 +19,26 @@ function App() {
           size="default"
           onClick={() => {
             setVisible(true);
+            console.log(theme);
           }}
         >
           Button
         </Button>
+        <div
+          style={{
+            fontWeight: "bold",
+            fontSize: "3rem",
+            verticalAlign: "middle",
+            marginTop: "200px",
+            backgroundColor: "#ffffff",
+            textAlign: "center",
+            width: "500px",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          This is a Paragraph!!!
+        </div>
         <Modal
           width={1000}
           height={500}
@@ -29,7 +47,7 @@ function App() {
             setVisible(false);
           }}
         >
-          Modal
+          <div style={{ color: "white", fontSize: "2rem" }}>Modal</div>
         </Modal>
       </div>
     </BgColorContext.Provider>

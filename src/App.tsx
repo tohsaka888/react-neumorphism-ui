02101ca908cda4dbfe983601a54e2ba5a200ef68
tohsaka888 <156132264@qq.com>
2022-04-02 15:34:59 +0800

    feat: test Button & Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { NeumorphismContainer } from "./styles/common.styles";
 import useBackgroundColor from "./hooks/useBackgroundColor";
 import { BgColorContext } from "./context/BgColorContext";
 import Button from "./components/Button";
+import Modal from "./components/Modal";
 
 function App() {
-  useBackgroundColor("#bf7272");
+  useBackgroundColor("#814747");
   const bgColor = useBackgroundColor();
+  const [visible, setVisible] = useState<boolean>(false);
   return (
     <BgColorContext.Provider value={bgColor}>
       <div className="App">
-        <NeumorphismContainer
-          width="50vw"
-          height="50vh"
-          bgColor={bgColor}
-          borderRadius="10px"
-          style={{ margin: "200px", padding: "20px" }}
+        <Button
+          style={{ color: "#ffffff" }}
+          size="default"
+          onClick={() => {
+            setVisible(true);
+          }}
         >
-          <Button style={{ color: "#2e2c2c" }} size="default">
-            Button
-          </Button>
-        </NeumorphismContainer>
+          Button
+        </Button>
+        <Modal
+          width={1000}
+          height={500}
+          visible={visible}
+          onClose={() => {
+            setVisible(false);
+          }}
+        >
+          Modal
+        </Modal>
       </div>
     </BgColorContext.Provider>
   );

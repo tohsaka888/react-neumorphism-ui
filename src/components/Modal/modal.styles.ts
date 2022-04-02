c30a5ca029f4b950be7a3747672515522ccaa48f
tohsaka888 <156132264@qq.com>
2022-04-02 15:35:48 +0800

    feat: modal style

---
+++ src/components/Modal/modal.styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ModalMain = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  padding: 16px;
+  text-align: left;
+`;

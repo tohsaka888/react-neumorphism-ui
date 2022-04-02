d7657c3422fca6c08f8df3b246250e276af0ba9d
tohsaka888 <156132264@qq.com>
2022-04-02 21:48:04 +0800

    feat: calculate luminance

---
+++ src/hooks/useLuminance.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+function useLuminance(): number {
+  const [luminance, setLuminance] = useState(0);
+  useEffect(() => {
+    const bodyBgColor = document.querySelector("body")?.style.backgroundColor;
+    const rgb = bodyBgColor?.match(/\d+(\.\d+)?/g)?.slice(0, 3) as string[];
+    setLuminance(+rgb[0] * 0.212656 + +rgb[1] * 0.715158 + +rgb[2] * 0.072186);
+    console.log(+rgb[0] * 0.212656 + +rgb[1] * 0.715158 + +rgb[2] * 0.072186);
+  }, []);
+  return luminance;
+}
+
+export default useLuminance;
